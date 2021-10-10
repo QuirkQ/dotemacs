a190a201a291ffaca74808d977f5b19646a48192
Quint Pieters <quint.pieters@Quints-MacBook-Pro.local>
2021-10-10 18:44:45 +0200

    Improved Ruby compatibility

--- init.el
+++ init.el
@@ -21,10 +21,27 @@
 (dolist (mode '(org-mode-hook
                 term-mode-hook
                 shell-mode-hook
+		magik-session-mode
+		ibuffer-mode
                 treemacs-mode-hook
                 eshell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
+(defun set-exec-path-from-shell-PATH ()
+  "Set up Emacs' `exec-path' and PATH environment variable to match
+that used by the user's shell.
+
+This is particularly useful under Mac OS X and macOS, where GUI
+apps are not started from a shell."
+  (interactive)
+  (let ((path-from-shell (replace-regexp-in-string
+			  "[ \t\n]*$" "" (shell-command-to-string
+					  "$SHELL --login -c 'echo $PATH'"
+						    ))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(set-exec-path-from-shell-PATH)
 
 ;; Set up the visible bell
 (setq visible-bell t)
@@ -60,25 +77,24 @@
          user-init-directory)
         (t "~/.emacs.d/")))
 
-
 (defun load-user-file (file)
   (interactive "f")
   "Load a file in current user's configuration directory"
   (load-file (expand-file-name file user-init-dir)))
 
+(use-package all-the-icons)
+
+(load-user-file "customizations/rbenv/rbenv.el")
+(require 'rbenv)
+(global-rbenv-mode)
+
 (load-user-file "packages.el")
+
 ;(load-user-file "mode-line.el")
 
 (set-face-attribute 'default nil :font "Hack" :height 120)
 
-(treemacs-load-theme "Default")
-
-(treemacs-modify-theme "Default"
-  :config
-  (progn
-	(treemacs-create-icon :file (expand-file-name "custom-icon/magik.png" user-emacs-directory)
-          :extensions ("magik"))
-	))
+(define-key enh-ruby-mode-map (kbd "<f3> b") 'ruby-send-buffer)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -86,13 +102,14 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(company magik-mode treemacs-persp treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil treemacs flycheck-pos-tip flycheck yasnippet git-gutter-fringe magit-todos magit-gitflow magit iflipb groovy-mode markdown-mode dockerfile-mode csv-mode ivy-rich counsel ivy use-package)))
+   '(smartparens smartparens-config company magik-mode treemacs-persp treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil treemacs flycheck-pos-tip flycheck yasnippet git-gutter-fringe magit-todos magit-gitflow magit iflipb groovy-mode markdown-mode dockerfile-mode csv-mode ivy-rich counsel ivy use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
 
 (provide 'init)
 ;;; init.el ends here
